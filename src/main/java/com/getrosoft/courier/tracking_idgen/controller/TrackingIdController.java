41667004f9840f384c32756b70c5f37a7bbd8db8
Balakrishnan <balakrishnan.nrb@gmail.com>
2025-03-17 21:14:34 +0530

    SOP Changed to Log
    SOP Changed to Log

--- src/main/java/com/getrosoft/courier/tracking_idgen/controller/TrackingIdController.java
+++ src/main/java/com/getrosoft/courier/tracking_idgen/controller/TrackingIdController.java
@@ -3,6 +3,8 @@ package com.getrosoft.courier.tracking_idgen.controller;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
+
+import org.apache.commons.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,9 @@ import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class TrackingIdController {
@@ -32,9 +35,9 @@ public class TrackingIdController {
     {    	
     	
         String trackingNumber = trackingIdService.generateTrackingNumber();
-        System.out.println("trackingNumber = "+trackingNumber);
+        log.info("trackingNumber = "+trackingNumber);
         
-        System.out.println("Time = "+request.getCreatedAt()+"-");
+        log.info("Time = "+request.getCreatedAt()+"-");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssX");
         //yyyy-MM-dd'T'HH:mmX
         Instant createdAt = Instant.parse(request.getCreatedAt().format(formatter));
